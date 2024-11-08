2a0278e2c68f2cd784c13f210dae1a6b7597ba23
mchswe <michael.sewell@laetottenham.ac.uk>
2024-11-08 12:26:18 +0000

    added... things

--- cats.py
+++ cats.py
@@ -5,18 +5,33 @@ class Cat: # cat entity
         self.colour = given_colour
         self.age = 1
         self.energy = 50
-        self.intelligence = 5
+        self.intelligence = 10
         self.weight = 5
 
     def train(self):
         print(f"{self.name} is training...")
         self.intelligence += 1
         self.energy -= 5
+        self.age += 0.2
 
     def feed(self):
         print (f"{self.name} is eating...")
         self.energy += 5
         self.weight += 1
+        self.age += 0.2
 
     def play(self):
         print(f"{self.name} is playing...")
+        self.energy -= 2
+        self.weight -= 1
+        self.age += 0.2
+
+    def sleep(self):
+        print(f"{self.name} is sleeping...")
+        self.energy += 5
+        self.age += 0.2
+
+    def view_stats(self):
+        print(f"{self.name} is {self.age} years old. \n Colour: {self.colour} \n Intelligence: {self.intelligence} \n Energy: {self.energy} \n Weight: {self.weight} ")
+
+    
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from cats import Cat
 
+print("Welcome to my Cat Game!")
+
+given_name = input("What is your Cat's name?")
+given_colour = input("What colour is your Cat?")
+
+cat1 = Cat(given_name, given_colour)
+print(cat1)
